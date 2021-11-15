296e49be7be5c23c69c491d769c36b379564dcbb
chafik <chafikdev92@gmail.com>
2021-11-15 14:33:58 +0100

    first commit

---
+++ starterOnly/modal.js
@@ -0,0 +1,39 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+// Close modal 
+const modalFermer = document.querySelector(".close");
+
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+
+// fermer la modale *****************************
+modalFermer.addEventListener("click", closeModal);
+
+function closeModal() {
+  modalbg.style.display = "none";
+}
+// *******
+
+
+
+
+
+
