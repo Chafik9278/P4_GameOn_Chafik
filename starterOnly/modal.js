c86df7408ab2db46dd3eb3732d1e1273bf1c33f0
chafik <chafikdev92@gmail.com>
2021-11-23 14:40:06 +0100

    fonction first name email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,15 +12,16 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 
-// Fermer modal
+// Fermer modal 
 const modalFermer = document.querySelector(".close");
 
-// Validation formulaire
+// Validation formulaire 
 const formValide = document.getElementById("form_valide");
 
-// Btn Fermer Modal Valide
+// Btn Fermer Modal Valide 
 const btnFermerModalValide = document.querySelector(".btn__fermerModal");
 // ********************************************
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -34,7 +35,7 @@ function launchModal() {
 // TODO : fermer la modale #1
 // ********************************************
 
-// // Fermer modal
+// // Fermer modal 
 // const modalFermer = document.querySelector(".close");
 
 modalFermer.addEventListener("click", closeModal);
@@ -44,23 +45,21 @@ function closeModal() {
 
   /* Modal formValide */
   formValide.style.display = "none";
+
 }
 // ********************************************
-
-// Validation formulaire
-const formValide = document.getElementById("form_valide");
-
-// // Btn Fermer Modal Valide
-// const btnFermerModalValide = document.querySelector(".btn__fermerModal");
 
 btnFermerModalValide.addEventListener("click", fermerModalValide);
 
 function fermerModalValide() {
+
   modalbg.style.display = "none";
 
   /* Modal formValide */
   formValide.style.display = "none";
+
 }
+
 
 // ********************************************
 // Implémenter entrées du formulaire #2
@@ -72,17 +71,25 @@ function fermerModalValide() {
 // "Vous devez vérifier que vous acceptez les termes et conditions."
 // "Vous devez entrer votre date de naissance."
 
+
 const firstName = document.getElementById("first");
+
 
 function formFirstName() {
   const errorFirstName = document.getElementById("error_first_name");
   const regexLetters = new RegExp(/^[A-Za-z-]+$/);
 
+
   if (firstName.value.length < 2) {
-    errorFirstName.innerHTML =
-      "Veuillez entrer 2 caractères ou plus pour le champ du nom";
+    errorFirstName.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
     errorFirstName.style.display = "block";
     return false;
+
+  } else if (!regexLetters.test(firstName.value)) {
+    errorFirstName.innerHTML = "Seul les caractéres alphanumériques sont acceptés, utilisé (-) pour les prénoms composés";
+    errorFirstName.style.display = "block";
+    return false
+
   } else {
     errorFirstName.style.display = "none";
 
@@ -91,6 +98,47 @@ function formFirstName() {
   }
 }
 
+function formLastName() {
+  const errorLastName = document.getElementById("error_last_name");
+  const regexLetters = new RegExp(/^[A-Za-z-]+$/);
+
+  if (lastName.value.length < 2) {
+    errorLastName.innerHTML = "Veuillez renseigner 2 caractères au minimumn";
+    errorLastName.style.display = "block";
+    return false;
+  } else if (!regexLetters.test(lastName.value)) {
+    errorLastName.innerHTML = "Seul les caractéres alphanumériques sont acceptés, utilisé (-) pour les noms composés";
+    errorLastName.style.display = "block";
+    return false
+   } else {
+    errorLastName.style.display = "none";
+
+    formValide.style.display = "block";
+    return true;
+  }
+}
+
+function formEmail() {
+  const errorEmail = document.getElementById("error_email");
+  const regexEmail = new RegExp(/\S+@\S+\.\S+/);
+  if (!email.value) {
+    errorEmail.innerHTML = "Veuillez renseigner votre email";
+    errorEmail.style.display = "block";
+    return false;
+  } else if (!regexEmail.test(email.value)) {
+    errorEmail.innerHTML = "Veuillez renseigner un email valide";
+    errorEmail.style.display = "block";
+    return false;
+  } else {
+    errorEmail.style.display = "none";
+
+    formValide.style.display = "block";
+    return true;
+  }
+}
+
+
+
 /* LORSQUE JE VALIDE  */
 const form_valide = document.getElementById("valide");
 
@@ -98,4 +146,25 @@ form_valide.addEventListener("submit", function (event) {
   event.preventDefault();
 
   formFirstName();
+
+
 });
+
+
+
+
+// ********************************************
+// Ajouter confirmation quand envoie réussi #4 
+// ********************************************
+
+
+//Après une validation réussie, inclure un message de confirmation de la soumission réussie pour l'utilisateur (ex. "Merci ! Votre réservation a été reçue.")
+
+
+// je sais pas encore comment faire 
+
+// ********************************************
+//Tests manuels #5 
+// ********************************************
+// Visualiser et tester l'interface utilisateur dans les dernières versions de Chrome et de Firefox, ainsi que dans les versions mobile et desktop. Corriger les erreurs d'affichage existantes.
+// Tester toutes les fonctionnalités des boutons et des entrées de formulaire (tester les valeurs correctes et incorrectes)
