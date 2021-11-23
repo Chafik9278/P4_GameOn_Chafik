ad277f3b2e2c4d1c8b699baaed4c8548aaec8816
chafik <chafikdev92@gmail.com>
2021-11-23 13:23:34 +0100

    btn fermer modal fin

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,19 +6,21 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
+// ********************************************
 // DOM Elements
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 
-// Fermer modal 
+// Fermer modal
 const modalFermer = document.querySelector(".close");
 
-// Validation formulaire 
+// Validation formulaire
 const formValide = document.getElementById("form_valide");
 
-// Btn Fermer Modal Valide 
+// Btn Fermer Modal Valide
 const btnFermerModalValide = document.querySelector(".btn__fermerModal");
+// ********************************************
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -31,6 +33,10 @@ function launchModal() {
 // ********************************************
 // TODO : fermer la modale #1
 // ********************************************
+
+// // Fermer modal
+// const modalFermer = document.querySelector(".close");
+
 modalFermer.addEventListener("click", closeModal);
 
 function closeModal() {
@@ -38,84 +44,58 @@ function closeModal() {
 
   /* Modal formValide */
   formValide.style.display = "none";
-
 }
 // ********************************************
+
+// Validation formulaire
+const formValide = document.getElementById("form_valide");
+
+// // Btn Fermer Modal Valide
+// const btnFermerModalValide = document.querySelector(".btn__fermerModal");
 
 btnFermerModalValide.addEventListener("click", fermerModalValide);
 
 function fermerModalValide() {
-
   modalbg.style.display = "none";
 
   /* Modal formValide */
   formValide.style.display = "none";
- 
 }
-
 
 // ********************************************
 // Implémenter entrées du formulaire #2
 
 // ********************************************
-//Des messages d'erreur spécifiques doivent apparaître sous l'entrée qui n'est pas correcte. Les messages d'erreur doivent s'afficher sous le champ de saisie associé. Exemples :
 
 // "Veuillez entrer 2 caractères ou plus pour le champ du nom."
 // "Vous devez choisir une option."
 // "Vous devez vérifier que vous acceptez les termes et conditions."
 // "Vous devez entrer votre date de naissance."
- 
 
 const firstName = document.getElementById("first");
 
+function formFirstName() {
+  const errorFirstName = document.getElementById("error_first_name");
+  const regexLetters = new RegExp(/^[A-Za-z-]+$/);
 
-  function formFirstName() {
-    const errorFirstName = document.getElementById("error_first_name");
-    const regexLetters = new RegExp(/^[A-Za-z-]+$/);
+  if (firstName.value.length < 2) {
+    errorFirstName.innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom";
+    errorFirstName.style.display = "block";
+    return false;
+  } else {
+    errorFirstName.style.display = "none";
 
-
-   if (firstName.value.length < 2) {
-      errorFirstName.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
-      errorFirstName.style.display = "block";
-      return false;
-
-
-    } else {
-      errorFirstName.style.display = "none";
-
-      formValide.style.display = "block";
-      return true;
-    }
+    formValide.style.display = "block";
+    return true;
   }
+}
 
-
-
-  /* LORSQUE JE VALIDE  */
+/* LORSQUE JE VALIDE  */
 const form_valide = document.getElementById("valide");
 
 form_valide.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  formFirstName(); 
-
-
+  formFirstName();
 });
-
-
-
-
-// ********************************************
-// Ajouter confirmation quand envoie réussi #4 
-// ********************************************
-
-
-//Après une validation réussie, inclure un message de confirmation de la soumission réussie pour l'utilisateur (ex. "Merci ! Votre réservation a été reçue.")
-
-
-// je sais pas encore comment faire 
-
-// ********************************************
-//Tests manuels #5 
-// ********************************************
- // Visualiser et tester l'interface utilisateur dans les dernières versions de Chrome et de Firefox, ainsi que dans les versions mobile et desktop. Corriger les erreurs d'affichage existantes.
-// Tester toutes les fonctionnalités des boutons et des entrées de formulaire (tester les valeurs correctes et incorrectes)
