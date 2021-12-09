a583fa16460fc4e2b9a608529ab877f7923ce0ba
chafik <chafikdev92@gmail.com>
2021-12-09 14:24:57 +0100

    revus des modals fermer

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,61 +6,42 @@ function editNav() {
     x.className = "topnav";
   }
 }
+
 // ********************************************
 // DOM Elements
-
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-
 // Fermer modal 
 const modalFermer = document.querySelector(".close");
-
 // Validation formulaire 
 const formValide = document.getElementById("form_valide");
-
 // Btn Fermer Modal Valide 
 const btnFermerModalValide = document.querySelector(".btn__fermerModal");
 // ********************************************
-
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 // ********************************************
 // TODO : fermer la modale #1
 // ********************************************
-
 // // Fermer modal 
 // const modalFermer = document.querySelector(".close");
-
 modalFermer.addEventListener("click", closeModal);
-
 function closeModal() {
   modalbg.style.display = "none";
-
   /* Modal formValide */
   formValide.style.display = "none";
-
 }
 // ********************************************
-
 btnFermerModalValide.addEventListener("click", fermerModalValide);
-
 function fermerModalValide() {
-
   modalbg.style.display = "none";
-
   /* Modal formValide */
   formValide.style.display = "none";
-
 }
-
-
 // ********************************************
 // Implémenter entrées du formulaire #2
 
@@ -81,6 +62,7 @@ function formFirstName() {
     errorFirstName.style.display = "block";
     return false;
 
+
   } else if (!regexLetters.test(firstName.value)) {
     errorFirstName.innerHTML = "Seul les caractéres alphanumériques sont acceptés, utilisé (-) pour les prénoms composés";
     errorFirstName.style.display = "block";
@@ -88,6 +70,7 @@ function formFirstName() {
 
   } else {
     errorFirstName.style.display = "none";
+    formValide.style.display = "block";
     return true;
   }
 }
@@ -125,14 +108,17 @@ const email = document.getElementById("email");
 function formEmail() {
   const errorEmail = document.getElementById("error_email");
   const regexEmail = new RegExp(/\S+@\S+\.\S+/);
+
   if (!email.value) {
     errorEmail.innerHTML = "Veuillez renseigner votre email";
     errorEmail.style.display = "block";
     return false;
+
   } else if (!regexEmail.test(email.value)) {
     errorEmail.innerHTML = "Veuillez renseigner un email valide";
     errorEmail.style.display = "block";
     return false;
+
   } else {
     errorEmail.style.display = "none";
 
@@ -148,13 +134,16 @@ const birthdate = document.getElementById("birthdate");
 
 function formBirthDate() {
   const errorBirthdate = document.getElementById("error_birthdate");
+
   if (!birthdate.value) {
     errorBirthdate.innerHTML = "Veuillez renseigner votre date d'anniversaire";
     errorBirthdate.style.display = "block";
     return false;
+
   } else if (birthdate.type !== "date") {
     errorBirthdate.innerHTML = "Veuillez renseigner une date";
     errorBirthdate.style.display = "block";
+
   } else {
     errorBirthdate.style.display = "none";
     return true;
@@ -170,15 +159,18 @@ const partEvent = document.getElementById("quantity");
 function formPartEvent() {
   const errorPartEvent = document.getElementById("error_partevent");
   const regexPartEvent = new RegExp("^[0-9][0-9]?$|^99$");
+
   if (!partEvent.value) {
     errorPartEvent.innerHTML =
       "Veuillez renseigner votre nombre de participation";
     errorPartEvent.style.display = "block";
     return false;
+
   } else if (!regexPartEvent.test(partEvent.value)) {
     errorPartEvent.innerHTML = "Veuillez renseigner un nombre entre 0 et 99";
     errorPartEvent.style.display = "block";
     return false;
+
   } else {
     errorPartEvent.style.display = "none";
     return true;
@@ -191,11 +183,13 @@ const checkCondition = document.getElementById("checkbox1");
 // ********************************************
 function formCheckCondition() {
   const errorCheckCondition = document.getElementById("error_checkCondition");
+
   if (!checkCondition.checked) {
     errorCheckCondition.innerHTML =
       "Veuillez accepter les conditions d'utilisation";
     errorCheckCondition.style.display = "block";
     return false;
+
   } else {
     errorCheckCondition.style.display = "none";
     return true;
@@ -210,12 +204,11 @@ const radioLocation = document.getElementsByName("location");
 // ********************************************
 function formRadioLocation() {
   const errorRadioLocation = document.getElementById("error_radioLocation");
-  const result = Array.from(radioLocation).filter(
-    (radio) => radio.checked === true
-  );
-  if (result.length > 0) {
+ 
+  if (radioLocation.length > 0) {
     errorRadioLocation.style.display = "none";
     return true;
+    
   } else {
     errorRadioLocation.innerHTML = "Veuillez selectionner une ville";
     errorRadioLocation.style.display = "block";
@@ -252,16 +245,11 @@ form_valide.addEventListener("submit", function (event) {
     formRadioLocation()
   ) {
     _
-  } else {
+  } else 
+  
+  {
     
     formValide.style.display = "none";
   }
 
 });
-
-
-// ********************************************
-//Tests manuels #5 
-// ********************************************
-// Visualiser et tester l'interface utilisateur dans les dernières versions de Chrome et de Firefox, ainsi que dans les versions mobile et desktop. Corriger les erreurs d'affichage existantes.
-// Tester toutes les fonctionnalités des boutons et des entrées de formulaire (tester les valeurs correctes et incorrectes)
